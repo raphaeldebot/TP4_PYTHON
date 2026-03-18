21724769d3e6a7b00e58e783788851aa7f01b48d
raphaeldebot <debot.raphael@gmail.com>
2026-03-18 15:33:21 +0100

    Ex4 Done

---
+++ Ex3.py
@@ -0,0 +1,7 @@
+n=input("Entrez un chiffre svp")
+i = 0
+somme = 0
+while i <= int(n) :
+        somme = i + somme
+        i+=1
+print(somme)
---
+++ Ex4.py
@@ -0,0 +1,5 @@
+word=input("Entrez un mot svp")
+i = 0
+while word[i:] != "" :
+   i+=1
+print(i)
